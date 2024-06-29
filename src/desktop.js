974bb94f8b24b033b76853cb7b973ff3ff2881f9
cypher2509 <you@example.com>
2024-06-29 04:54:01 -0230

    jun29

--- src/desktop.js
+++ src/desktop.js
@@ -33,11 +33,9 @@ function update(event){
 	container.style.setProperty("--y", ratioY);
 }// update
 
-
-
-var mousedownat=0;
 const Desktop = ({ className = "" }) => {
     const{ref: myRef ,inView: aboutIsVisible} = useInView();
+    const{ref: myproject ,inView: myprojectIsVisible} = useInView();
     const{ref: bigbull ,inView: bigbullIsVisible} = useInView();
     const{ref: cantstop ,inView: cantstopIsVisible} = useInView();
     const{ref: zombiechase ,inView: zombiechaseIsVisible} = useInView();
@@ -46,6 +44,7 @@ const Desktop = ({ className = "" }) => {
     const track = document.getElementById('image-track')
 
     useEffect(() => {
+       
         window.addEventListener("mousemove", update);
         window.onmousedown = e=>{
           for(const track of tracks){
@@ -90,6 +89,7 @@ const Desktop = ({ className = "" }) => {
   <div >
 
 <link href='https://fonts.googleapis.com/css?family=Vibur:400' rel='stylesheet' type='text/css'/>
+<link href="https://fonts.googleapis.com/css2?family=Playwrite+DE+Grund:wght@100..400&display=swap" rel="stylesheet"></link>
     <div className="loader-page">
         <Loader/>
         <Cup/>
@@ -104,7 +104,7 @@ const Desktop = ({ className = "" }) => {
     <Parallax pages={8.35} style={{height:"750px"}}>
 
       <ParallaxLayer sticky={{start:0,end:1}}>
-      <a href="mailto:lvpanchal@mun.ca?Subject=Hi%20Lokesh%21%20I%20would%20like%20to%20hire%20you" className="hire-me" style={{textDecoration:"none",color:"black"}}>
+      <a href="mailto:lvpanchal@mun.ca?Subject=Hi%20Lokesh%21%20I%20would%20like%20to%20hire%20you" className="hire-me" id= "hire-me"style={{textDecoration:"none"}}>
   hire me!
 </a>
 
@@ -140,7 +140,15 @@ const Desktop = ({ className = "" }) => {
       </ParallaxLayer>
     <ParallaxLayer offset={0} speed={1.5}>
       <div className="desktop-1-item" />
-      <div className="scroll">scroll</div>
+      <div className="scroll"> 
+      <span className="scroll-letter" style={{"--i":1}}>s</span>
+      <span className="scroll-letter" style={{"--i":2}}>c</span>
+      <span className="scroll-letter" style={{"--i":3}}>r</span>
+      <span className="scroll-letter" style={{"--i":4}}>o</span>
+      <span className="scroll-letter" style={{"--i":5}}>l</span>
+      <span className="scroll-letter" style={{"--i":6}}>l</span>
+
+      </div>
       </ParallaxLayer>
 
       <ParallaxLayer offset={1} speed={1.4} >
@@ -175,18 +183,18 @@ const Desktop = ({ className = "" }) => {
     </ParallaxLayer>
     <div className="projects-section">
           <ParallaxLayer offset={2.5}>
-            <div style={{display:"flex", alignContent:"center"}}>
-              <img className ="cloud1" src="/images/myproject/bg.png" style={{width:"46rem"}}></img>
-              <img className="parts" id="part1" src="/images/myproject/part1.png"></img>
-              <img className="parts" id="part2" src="/images/myproject/part2.png"></img>
-              <img className="parts" id="part3" src="/images/myproject/part3.png"></img>
-              <img className="parts" id="part4" src="/images/myproject/part4.png"></img>
-              <img className="parts" id="part5" src="/images/myproject/part5.png"></img>
-              <img className="parts" id="part6" src="/images/myproject/part6.png"></img>
-              <img className="parts" id="part7" src="/images/myproject/part7.png"></img>
-              <img className="parts" id="part8" src="/images/myproject/part8.png"></img>
-              <img className="parts" id="part9" src="/images/myproject/part9.png"></img>
-              <img className="parts" id="part10" src="/images/myproject/part10.png"></img>
+            <div ref={myproject} style={{display:"flex", alignContent:"center"}}>
+              <img alt= "" id="bg"className ="cloud1" src="/images/myproject/bg.png" style={{width:"46rem"}}></img>
+              <img alt= "" className={`parts part1 ${myprojectIsVisible? 'in-view' :''}`} id="part1" src="/images/myproject/part1.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part2" src="/images/myproject/part2.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part3" src="/images/myproject/part3.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part4" src="/images/myproject/part4.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part5" src="/images/myproject/part5.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part6" src="/images/myproject/part6.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part7" src="/images/myproject/part7.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part8" src="/images/myproject/part8.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part9" src="/images/myproject/part9.png"></img>
+              <img alt= "" className={`parts ${myprojectIsVisible? 'in-view' :''}`} id="part10" src="/images/myproject/part10.png"></img>
 
 
 
@@ -204,6 +212,12 @@ const Desktop = ({ className = "" }) => {
             
           {/* <div  className="sliding-img"> */}
               <div ref={bigbull} className="image-track" data-mouse-down-at="0" data-prev-percentage= "0" data-visible ={`${bigbullIsVisible? 1 :0}`} draggable="false">
+              <div class="arrow">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+              </div>
+
                 <img alt="" className="image" src="/images/bigbull/1.png" draggable="false"/>
                 <img alt=""  className="image" src="/images/bigbull/2.png"draggable="false"/>
                 <img alt="" className="image"  src="/images/bigbull/3.png"draggable="false"/>
@@ -238,8 +252,8 @@ const Desktop = ({ className = "" }) => {
                         </svg>
                       </div>
                       <div class="cube">
-                        <span class="side front">hover me</span>
-                        <span class="side top">check it on github</span>
+                        <span class="side front">check it</span>
+                        <span class="side top">on github</span>
                       </div>
                     </div>
                   </a>
@@ -252,6 +266,11 @@ const Desktop = ({ className = "" }) => {
           <ParallaxLayer offset={5}>
           <div className="project">
           <div ref={cantstop} id="cantstop" className="image-track" data-mouse-down-at="0" data-prev-percentage= "0" data-visible={`${cantstopIsVisible? 1:0}`} draggable="false">
+                <div class="arrow">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
                 <img alt="" className="image" src="/images/cantstop/1.png" draggable="false"/>
                 <img alt=""  className="image" src="/images/cantstop/2.png"draggable="false"/>
                 <img alt="" className="image"  src="/images/cantstop/3.png"draggable="false"/>
@@ -288,8 +307,8 @@ const Desktop = ({ className = "" }) => {
                         </svg>
                       </div>
                       <div class="cube">
-                        <span class="side front">hover me</span>
-                        <span class="side top">check it on github</span>
+                        <span class="side front">check it</span>
+                        <span class="side top">on github</span>
                       </div>
                     </div>
                 </a>
@@ -324,6 +343,11 @@ const Desktop = ({ className = "" }) => {
                 
             </div> */}
               <div ref={zombiechase} id="zombiechase" className="image-track" data-mouse-down-at="0" data-prev-percentage= "0" draggable="false" data-visible={`${zombiechaseIsVisible? 1 :0}`}>
+                <div class="arrow">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
                 <img alt="" className="image" src="/images/zombiechase/1.png" draggable="false"/>
                 <img alt=""  className="image" src="/images/zombiechase/2.png"draggable="false"/>
                 <img alt="" className="image"  src="/images/zombiechase/3.png"draggable="false"/>
@@ -350,16 +374,11 @@ const Desktop = ({ className = "" }) => {
               <a href="https://drive.google.com/drive/u/1/folders/1wlHiXTjLhlzzFs8ukyecNVfQJmLCbBLx">
               <div class="button-icon">
                       <div class="icon">
-                        <svg viewBox="0 0 24 24">
-                          <path
-                            d="M12 0.296997C5.37 0.296997 0 5.67 0 12.297C0 17.6 3.438 22.097 8.205 23.682C8.805 23.795 9.025 23.424 9.025 23.105C9.025 22.82 9.015 22.065 9.01 21.065C5.672 21.789 4.968 19.455 4.968 19.455C4.422 18.07 3.633 17.7 3.633 17.7C2.546 16.956 3.717 16.971 3.717 16.971C4.922 17.055 5.555 18.207 5.555 18.207C6.625 20.042 8.364 19.512 9.05 19.205C9.158 18.429 9.467 17.9 9.81 17.6C7.145 17.3 4.344 16.268 4.344 11.67C4.344 10.36 4.809 9.29 5.579 8.45C5.444 8.147 5.039 6.927 5.684 5.274C5.684 5.274 6.689 4.952 8.984 6.504C9.944 6.237 10.964 6.105 11.984 6.099C13.004 6.105 14.024 6.237 14.984 6.504C17.264 4.952 18.269 5.274 18.269 5.274C18.914 6.927 18.509 8.147 18.389 8.45C19.154 9.29 19.619 10.36 19.619 11.67C19.619 16.28 16.814 17.295 14.144 17.59C14.564 17.95 14.954 18.686 14.954 19.81C14.954 21.416 14.939 22.706 14.939 23.096C14.939 23.411 15.149 23.786 15.764 23.666C20.565 22.092 24 17.592 24 12.297C24 5.67 18.627 0.296997 12 0.296997Z"
-                            fill="#222229"
-                          ></path>
-                        </svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" id="google-drive"><path d="m15.467 10-4.8-9H5.333l4.8 9zM4.747 2.026 0 10.333 2.667 15l4.655-8.146zM6.104 11l-2.286 4H13.5l2.5-4z"></path></svg>
                       </div>
                       <div class="cube">
-                        <span class="side front">hover me</span>
-                        <span class="side top">check it on Drive</span>
+                        <span class="side front">check it</span>
+                        <span class="side top">on Drive</span>
                       </div>
                     </div>
               </a>
@@ -382,7 +401,7 @@ const Desktop = ({ className = "" }) => {
     {/* <div ref={myRef} className={`about ${aboutIsVisible? 'aboutAnimate' :''}`}  >
       {aboutIsVisible?'yes':'no'}
            </div> */}
-      <div className="skills-heading">Skills <hr/></div>
+      <div className="skills-heading">My skills <hr/></div>
       <div className="skills">
         <img className="skill-img" src="/images/skills/0.png"></img>
         <img className="skill-img" src="/images/skills/1.png"></img>
